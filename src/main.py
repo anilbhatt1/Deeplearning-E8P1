8dda926a66a7490a3f58e1b29b0e9657e8336da2
anilbhatt1 <anilaknb@gmail.com>
2023-02-24 11:27:18 +0530

    Added S9_Basic_Self_Attention

---
+++ src/main.py
@@ -0,0 +1,98 @@
+import torch
+import torch.optim as optim
+import matplotlib.pyplot as plt
+cuda = torch.cuda.is_available()
+from torchsummary import summary
+from torch.optim.lr_scheduler import ReduceLROnPlateau
+from torch.optim.lr_scheduler import OneCycleLR
+
+from models import *
+from utilities import *
+from train_loss import *
+from test_loss import *
+
+def s9_run_main(num_epochs, maxlr):
+
+    # Data Augmentation & data loader stuff to be handled
+    batch_size = 512
+    trainloader, testloader = S9_CIFAR10_data_prep(batch_size)
+
+    # Creating tensorboard writer
+    img_save_path = '/content/gdrive/MyDrive/EVA8_P1_S9/'
+    tb_writer = create_tensorboard_writer(img_save_path)
+
+    # Creating plot object
+    plot = cifar10_plots(img_save_path, tb_writer)
+
+    # Displaying train data
+    plot.plot_cifar10_train_imgs(trainloader)
+
+    # Displaying torch summary
+    use_cuda = torch.cuda.is_available()
+    device = torch.device("cuda" if use_cuda else "cpu")
+    model = basic_attn_model().to(device)
+    summary(model, input_size=(3, 32, 32))
+
+    # Adding model graph to tensor-board
+    img = torch.ones(1, 3, 32, 32)
+    img = img.to(device)
+    tb_writer.add_graph(model, img)
+
+    # Note : Plot not coming out correctly when ran via module. Hence will find maxlr outside the main & pass it as arg
+
+    # Finding Max LR using range test
+    #model = basic_attn_model().to("cuda")
+    #lrfinder = LRRangeFinder(model=model, epochs=3, start_lr=1e-2, end_lr=1e-1, tb_writer= tb_writer,
+    #                         dataloader=trainloader, device=device, img_save_path=img_save_path)
+    #max_lr = lrfinder.findLR()
+    max_lr = maxlr
+    print(f'max_lr is {max_lr}')
+
+    # Training the model for fixed epochs
+    EPOCHS = num_epochs
+    model = basic_attn_model().to(device)
+    optimizer = optim.Adam(model.parameters(), lr=max_lr)
+    scheduler = OneCycleLR(optimizer, max_lr=max_lr, steps_per_epoch=len(trainloader), epochs=EPOCHS, 
+                           pct_start = 5/24, div_factor=10, final_div_factor=1)
+    stats = ctr()
+    train = train_losses(model, device, trainloader, stats, optimizer, EPOCHS)
+    test  = test_losses(model, device, testloader, stats, EPOCHS)
+    print(f'Initial LR : {scheduler.get_lr()}')
+    print(f'Total steps: {scheduler.total_steps}')
+
+    for epoch in range(EPOCHS):
+        print(f'EPOCH: {epoch+1}')
+        train.s9_train(epoch, scheduler, tb_writer, L1_factor=0.0005)
+        test.s9_test(epoch, tb_writer)
+
+    details = counters
+
+    # Plot OneCycle-LR Curve
+    plot_onecyclelr_curve(details, img_save_path)
+
+    # Displaying 20 misclassified images
+    num_images = 25
+    plot.plot_cifar10_misclassified(details, num_images)
+
+    # Plotting train & test accuracies and losses
+    plt.figure(figsize=(12, 8))
+    plt.title(f"Train Losses")
+    plt.plot(details['train_loss'])
+    plt.savefig(f'{img_save_path}train_loss.jpg')
+
+    plt.figure(figsize=(12,8))
+    plt.title(f"Train Accuracy")
+    plt.plot(details['train_acc'])
+    plt.savefig(f'{img_save_path}train_acc.jpg')
+
+    plt.figure(figsize=(12,8))
+    plt.title(f"Test Losses")
+    plt.plot(details['test_loss'])
+    plt.savefig(f'{img_save_path}test_loss.jpg')
+
+    plt.figure(figsize=(12,8))
+    plt.title(f"Test Accuracy")
+    plt.plot(details['test_acc'])
+    plt.savefig(f'{img_save_path}test_acc.jpg')
+
+    return f' s9_run_main() ended successfully '
---
+++ src/train_loss.py
@@ -0,0 +1,232 @@
+from tqdm import tqdm
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+
+class train_losses():
+    def __init__(self, model, device, train_loader, train_stats, optimizer, total_epochs):
+        self.model = model
+        self.device = device
+        self.train_loader = train_loader
+        self.stats = train_stats
+        self.optimizer = optimizer
+        self.total_epochs = total_epochs
+
+    def s6_train(self, current_epoch, L1_factor=0):
+        self.model.train()
+        pbar = tqdm(self.train_loader)
+        train_loss, correct, processed = 0, 0, 0
+        for batch_idx, (data, target) in enumerate(pbar):
+            data, target = data.to(self.device), target.to(self.device)
+            self.optimizer.zero_grad()
+            y_pred = self.model(data)
+            train_loss = F.nll_loss(y_pred, target)
+
+            # Updating train loss with L1 loss
+            L1_Crit = nn.L1Loss(size_average=None, reduce=None, reduction='mean')
+            reg_loss = 0
+            for param in self.model.parameters():
+                zero_vector = torch.rand_like(param) * 0
+                reg_loss += L1_Crit(param, zero_vector)
+            train_loss += L1_factor * reg_loss
+
+            train_loss.backward()
+            self.optimizer.step()
+
+            pred = y_pred.argmax(dim=1, keepdim=True)  # get the index of the max log-probability
+            correct += pred.eq(target.view_as(pred)).sum().item()
+            processed += len(data)
+
+            pbar.set_description(
+                desc=f'Loss={train_loss.item()} Batch_id={batch_idx} Train Accuracy={100 * correct / processed:0.2f}')
+
+            train_loss /= processed
+            self.stats.append_loss(round(train_loss.item(), 6))
+
+        train_acc = round((100. * correct / len(self.train_loader.dataset)), 2)
+        self.stats.append_acc(train_acc)
+
+        print(
+            f'Train set: Epoch : {current_epoch}/{self.total_epochs} Average loss: {train_loss :.4f}, Train Accuracy: {train_acc}')
+
+    def s7_train(self, current_epoch, L1_factor=0):
+        self.model.train()
+        pbar = tqdm(self.train_loader)
+        train_loss, correct, processed = 0, 0, 0
+        for batch_idx, (data, target) in enumerate(pbar):
+            data, target = data.to(self.device), target.to(self.device)
+            self.optimizer.zero_grad()
+            y_pred = self.model(data)
+            train_loss = F.nll_loss(y_pred, target)
+
+            # Updating train loss with L1 loss
+            L1_Crit = nn.L1Loss(size_average=None, reduce=None, reduction='mean')
+            reg_loss = 0
+            for param in self.model.parameters():
+                zero_vector = torch.rand_like(param) * 0
+                reg_loss += L1_Crit(param, zero_vector)
+            train_loss += L1_factor * reg_loss
+
+            train_loss.backward()
+            self.optimizer.step()
+
+            pred = y_pred.argmax(dim=1, keepdim=True)  # get the index of the max log-probability
+            correct += pred.eq(target.view_as(pred)).sum().item()
+            processed += len(data)
+
+            pbar.set_description(
+                desc=f'Loss={train_loss.item()} Batch_id={batch_idx} Train Accuracy={100 * correct / processed:0.2f}')
+
+            train_loss /= processed
+            self.stats(round(train_loss.item(), 6), 'train_loss')
+
+        train_acc = round((100. * correct / len(self.train_loader.dataset)), 2)
+        self.stats(train_acc, 'train_acc')
+
+        print(
+            f'Train set: Epoch : {current_epoch}/{self.total_epochs} Average loss: {train_loss :.4f}, Train Accuracy: {train_acc}')
+
+    def s8_train(self, current_epoch, scheduler, tb_writer, L1_factor=0):
+        self.model.train()
+        pbar = tqdm(self.train_loader)
+        train_loss, correct, processed = 0, 0, 0
+        for batch_idx, (data, target) in enumerate(pbar):
+            data, target = data.to(self.device), target.to(self.device)
+            self.optimizer.zero_grad()
+            y_pred = self.model(data)
+            train_loss = F.nll_loss(y_pred, target)
+
+            # Updating train loss with L1 loss
+            L1_Crit = nn.L1Loss(size_average=None, reduce=None, reduction='mean')
+            reg_loss = 0
+            for param in self.model.parameters():
+                zero_vector = torch.rand_like(param) * 0
+                reg_loss += L1_Crit(param, zero_vector)
+            train_loss += L1_factor * reg_loss
+
+            train_loss.backward()
+            self.optimizer.step()
+
+            lr = 0
+            if scheduler and not (isinstance(scheduler, torch.optim.lr_scheduler.ReduceLROnPlateau)):
+                lr = scheduler.get_last_lr()[0]  # Won't work for ReduceLRonPlateau
+            else:
+                lr = self.optimizer.param_groups[0]['lr']
+
+            pred = y_pred.argmax(dim=1, keepdim=True)  # get the index of the max log-probability
+            correct += pred.eq(target.view_as(pred)).sum().item()
+            processed += len(data)
+
+            pbar.set_description(
+                desc=f'Loss={train_loss.item()} Batch_id={batch_idx} Train Accuracy={100 * correct / processed:0.2f}')
+
+            tb_writer.add_scalar('Train loss', round(train_loss.item(), 4), global_step=current_epoch)
+
+            self.stats(round(train_loss.item(), 6), 'train_loss')
+
+        train_acc = round((100. * correct / len(self.train_loader.dataset)), 2)
+        self.stats(train_acc, 'train_acc')
+        tb_writer.add_scalar('Acc/Train', train_acc, global_step=current_epoch)
+
+        print(
+            f'Train set: Epoch : {current_epoch}/{self.total_epochs} Average loss: {train_loss :.4f}, Train Accuracy: {train_acc}')
+
+    def s9_train(self, current_epoch, scheduler, tb_writer, L1_factor=0):
+        self.model.train()
+        pbar = tqdm(self.train_loader)
+        train_loss, correct, processed = 0, 0, 0
+        for batch_idx, (data, target) in enumerate(pbar):
+            data, target = data.to(self.device), target.to(self.device)
+            self.optimizer.zero_grad()
+            y_pred = self.model(data)
+            train_loss = F.nll_loss(y_pred, target)
+
+            # Updating train loss with L1 loss
+            L1_Crit = nn.L1Loss(size_average=None, reduce=None, reduction='mean')
+            reg_loss = 0
+            for param in self.model.parameters():
+                zero_vector = torch.rand_like(param) * 0
+                reg_loss += L1_Crit(param, zero_vector)
+            train_loss += L1_factor * reg_loss
+
+            train_loss.backward()
+            self.optimizer.step()
+
+            lr = 0
+            if scheduler and not (isinstance(scheduler, torch.optim.lr_scheduler.ReduceLROnPlateau)):
+                lr = scheduler.get_last_lr()[0]  # Won't work for ReduceLRonPlateau
+            else:
+                lr = self.optimizer.param_groups[0]['lr']
+
+            pred = y_pred.argmax(dim=1, keepdim=True)  # get the index of the max log-probability
+            correct += pred.eq(target.view_as(pred)).sum().item()
+            processed += len(data)
+
+            pbar.set_description(desc=f'Loss={train_loss.item() :0.4f} Batch={batch_idx} Train Acc={100 * correct / processed:0.2f}')
+
+            train_iter = current_epoch * len(self.train_loader) + (batch_idx + 1)  # To find the iteration at which training is
+
+            tb_writer.add_scalar('Train loss', round(train_loss.item(), 4), global_step=train_iter)
+            tb_writer.add_scalar('LR', round(lr, 4), global_step=train_iter)
+
+            self.stats(round(train_loss.item(), 6), 'train_loss')
+            self.stats(lr, 'train_lr')
+
+            if scheduler:
+                scheduler.step()
+
+        train_acc = round((100. * correct / len(self.train_loader.dataset)), 2)
+        self.stats(train_acc, 'train_acc')
+        tb_writer.add_scalar('Acc/Train', train_acc, global_step=current_epoch)
+
+        print(f'Train set: Epoch : {current_epoch + 1}/{self.total_epochs} Average loss: {train_loss :.4f}, Train Accuracy: {train_acc}')
+
+    def s10_train(self, current_epoch, scheduler, tb_writer, L1_factor=0):
+        self.model.train()
+        pbar = tqdm(self.train_loader)
+        train_loss, correct, processed = 0, 0, 0
+        for batch_idx, (data, target) in enumerate(pbar):
+            data, target = data.to(self.device), target.to(self.device)
+            self.optimizer.zero_grad()
+            y_pred = self.model(data)
+            train_loss = F.nll_loss(y_pred, target)
+
+            # Updating train loss with L1 loss
+            L1_Crit = nn.L1Loss(size_average=None, reduce=None, reduction='mean')
+            reg_loss = 0
+            for param in self.model.parameters():
+                zero_vector = torch.rand_like(param) * 0
+                reg_loss += L1_Crit(param, zero_vector)
+            train_loss += L1_factor * reg_loss
+
+            train_loss.backward()
+            self.optimizer.step()
+
+            lr = 0
+            if scheduler and not (isinstance(scheduler, torch.optim.lr_scheduler.ReduceLROnPlateau)):
+                lr = scheduler.get_last_lr()[0]  # Won't work for ReduceLRonPlateau
+            else:
+                lr = self.optimizer.param_groups[0]['lr']
+
+            pred = y_pred.argmax(dim=1, keepdim=True)  # get the index of the max log-probability
+            correct += pred.eq(target.view_as(pred)).sum().item()
+            processed += len(data)
+
+            pbar.set_description(desc=f'Loss={train_loss.item() :0.4f} Batch={batch_idx} Train Acc={100 * correct / processed:0.2f}')
+
+            train_iter = current_epoch * len(self.train_loader) + (batch_idx + 1)  # To find the iteration at which training is
+
+            tb_writer.add_scalar('Train loss', round(train_loss.item(), 4), global_step=train_iter)
+            tb_writer.add_scalar('LR', round(lr, 4), global_step=train_iter)
+
+            self.stats(round(train_loss.item(), 6), 'train_loss')
+            self.stats(lr, 'train_lr')
+
+            if scheduler:
+                scheduler.step()
+
+        train_acc = round((100. * correct / len(self.train_loader.dataset)), 2)
+        self.stats(train_acc, 'train_acc')
+        tb_writer.add_scalar('Acc/Train', train_acc, global_step=current_epoch)
+
+        print(f'Train set: Epoch : {current_epoch + 1}/{self.total_epochs} Average loss: {train_loss :.4f}, Train Accuracy: {train_acc}')
